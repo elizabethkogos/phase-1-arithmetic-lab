1a1c12bca0cbe96d2cfff65ce2ba00a2070b361b
elizabethkogos <lizkogos@gmail.com>
2022-08-16 13:51:38 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const multiply = (num1,num2) => {
+
+return num1 * num2;  
+};
+let resulMultiply = multiply(31, 2);
+console.log(resulMultiply);
+
+Math.floor(Math.random() *  max >0 );
+
+const mod = (num3,num4)=> {
+
+    return num3 % num4;  
+    };
+    let resulMod =(56,2)
+    console.log(resulMultiply);
+
+    
+
+
 
 // Write your code here
